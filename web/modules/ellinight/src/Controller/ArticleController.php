3056dd8c018f2f372fc91d99afd56b8db0d98106
Frorex Studio <81986912+FrorexStudio@users.noreply.github.com>
2022-01-11 18:17:51 +0200

    initial commit

---
+++ web/modules/ellinight/src/Controller/ArticleController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Drupal\ellinight\Controller;
+
+class ArticleController {
+
+  public function page() {
+
+    return array(
+      '#title' => 'Hello! You can add here a photo of your cat.'
+    );
+  }
+}
